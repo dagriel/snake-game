a27f5182c908e804dbb06b0f183f68c81298953c
dagriel <dagriel@gmail.com>
2021-01-11 16:32:44 -0300

    defines the directions of the snake and prevents it from disappearing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let context = canvas.getContext("2d"); //....
 let box = 32;
 
 let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+
 snake[0] ={
     x: 8 * box,
     y: 8 * box
@@ -22,7 +23,22 @@ function criarCobrinha (){
     }
 }
 
-function    iniciarJogo(){
+document.addEventListener("keydown",update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+function iniciarJogo(){
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -31,8 +47,8 @@ function    iniciarJogo(){
 
     if (direction == "right") snakeX += box;
     if (direction == "left") snakeX -= box;
-    if (direction == "up") snakeY += box;
-    if (direction == "down") snakeY -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -45,4 +61,3 @@ function    iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo,100); //100 milliseconds
-
